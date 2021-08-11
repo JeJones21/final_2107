34e907ca3dc20a961fac79f6b125f69bab3be7fb
Jessica Jones <daddehjones@gmail.com>
2021-08-11 12:21:55 -0400

    Add zero bids expectations.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -12,9 +12,17 @@ RSpec.describe Item do
       expect(item2).to be_a(Item)
       expect(item1.name).to eq('Chalkware Piggy Bank')
     end
+  end
 
+  context 'Object Methods do' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
 
-
-
-end
+    it "starts with zero bids" do
+      expect(item1.bids).to eq({})
+    end
+  end
 end
