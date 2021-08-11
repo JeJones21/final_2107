4bb4327cef9e55c1306f71f8c4b4fc4dfe1b64f8
Jessica Jones <daddehjones@gmail.com>
2021-08-11 12:22:19 -0400

    Add bids hash and current_high_bid.

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,18 @@
 class Item
 
-  attr_reader :name
+  attr_reader :name,
+              :bids
+
   def initialize(name)
     @name = name
+    @bids = Hash.new(0)
+  end
+
+  def add_bid(attendee, bid)
+    @bids[attendee] += bid
+  end
+
+  def current_high_bid
+    @bids.values.max
   end
 end
