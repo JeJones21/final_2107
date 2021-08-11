1e0f170e55175618055cdf8c2cd2f5ef8f0c55f0
Jessica Jones <daddehjones@gmail.com>
2021-08-11 13:16:42 -0400

    Did not finish info

--- lib/auction.rb
+++ lib/auction.rb
@@ -21,4 +21,57 @@ class Auction
   # def add_bid(attendee, amount)
   #   @bids[attendee] += amount
   # end
+
+  def unpopular_items
+    @items.find_all do |item|
+      item.bids.length < 1
+    end
+  end
+
+  # def potential_revenue
+  #   @items.find_all do |item|
+  #     item.bids.max
+  #   end
+  # end
+
+  def bid_on
+    #helper method
+    @items.find_all do |item|
+      item.bids.length > 0
+    end
+  end
+
+  def potential_revenue
+    bid_on.sum {|item| item.current_high_bid}
+  end
+
+  def bidders
+    #need to list the attendees by name
+    # @attendees.map do |attendee|
+    #   attendee.name
+    #
+    bidders = bid_on.flat_map do |item|
+      item.bids.keys
+    end
+    bidders.map do |bidder|
+      bidder.name
+    end.uniq
+  end
+
+  def bidder_info
+    #need to return a nested hash with attendees as keys and budget and items as values.
+    expected = Hash.new([])
+
+    @attendee.each do |attendee|
+      attendee.name.each do |name|
+        if expected[attendee.name].nil?
+          expected[attendee.name] = [budget]
+        else
+          expected[attendee.actor].push(item
+        end
+      end
+    end
+    #not finished
+    expected
+  end
 end
